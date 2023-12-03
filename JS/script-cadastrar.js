66a88003b4c72c6a3b048e23f67b1908e6f1f38e
Gabriel Nicolau de Sousa <gabrielnicolllau1@gmail.com>
2023-12-03 18:15:08 -0300

    mudar a cor das label

--- JS/script-cadastrar.js
+++ JS/script-cadastrar.js
@@ -1,7 +1,7 @@
 let nome = document.querySelector("#nome");
 let labelNome = document.querySelector("#labelNome");
 
-let usuario = document.querySelector("#labelUsuario");
+let usuario = document.querySelector("#usuario");
 let labelUsuario = document.querySelector("#labelUsuario");
 
 let senha = document.querySelector("#senha");
@@ -10,11 +10,36 @@ let labelSenha = document.querySelector("#labelSenha");
 let confimarSenha = document.querySelector("#confimarSenha");
 let labelConfimarSenha = document.querySelector("#labelConfimarSenha");
 
-nome.addEventListener("keyup", ()=> {
-    if(nome.value.length <= 2) {
+// Mudar a cor nos campos
+nome.addEventListener("keyup", () => {
+    if (nome.value.length <= 2) {
         labelNome.setAttribute("style", "color: red");
     } else {
         labelNome.setAttribute("style", "color: green");
+    }
+});
+
+usuario.addEventListener("keyup", () => {
+    if (usuario.value.length <= 4) {
+        labelUsuario.setAttribute("style", "color: red");
+    } else {
+        labelUsuario.setAttribute("style", "color: green");
+    }
+});
+
+senha.addEventListener("keyup", () => {
+    if (senha.value.length <= 5) {
+        labelSenha.setAttribute("style", "color: red");
+    } else {
+        labelSenha.setAttribute("style", "color: green");
+    }
+});
+
+confimarSenha.addEventListener("keyup", () => {
+    if (senha.value !== confimarSenha.value) {
+        labelConfimarSenha.setAttribute("style", "color: red");
+    } else {
+        labelConfimarSenha.setAttribute("style", "color: green");
     }
 });
 
